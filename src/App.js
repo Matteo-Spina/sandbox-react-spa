8edd5e0923b6fd9e5cbe3c6e7b46852408f3bfbf
Matteo <matteo.spina@hotmail.com>
2021-10-14 11:12:16 +0200

    disable vendor prefixes

--- src/App.js
+++ src/App.js
@@ -2,29 +2,32 @@ import "./App.css";
 import GenericComponent from "./GenericComponent";
 import ArticleBlock from "./Article";
 import OrbitSystem from "./OrbitSystem";
+import { StyleSheetManager } from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="page-header"></header>
-      {/* <!-- grid --> */}
-      <main>
-        <div className="row">
-          <OrbitSystem />
-          <ArticleBlock />          
-        </div>
-        <div className="row">
-          <ArticleBlock />
-          <GenericComponent />
-        </div>
-        <div className="row">
-          <GenericComponent />
-          <ArticleBlock />
-        </div>
-      </main>
-      {/* <!-- end grid --> */}
-      <footer className="page-footer"></footer>
-    </div>
+    <StyleSheetManager disableVendorPrefixes>
+      <div className="App">
+        <header className="page-header"></header>
+        {/* <!-- grid --> */}
+        <main>
+          <div className="row">
+            <OrbitSystem />
+            <ArticleBlock />
+          </div>
+          <div className="row">
+            <ArticleBlock />
+            <GenericComponent />
+          </div>
+          <div className="row">
+            <GenericComponent />
+            <ArticleBlock />
+          </div>
+        </main>
+        {/* <!-- end grid --> */}
+        <footer className="page-footer"></footer>
+      </div>
+    </StyleSheetManager>
   );
 }
 
