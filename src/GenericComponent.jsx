63a820e18f0f1f88fa5067e5030b110ca3104274
Matteo <matteo.spina@hotmail.com>
2021-10-14 13:14:54 +0200

    debug decorations

--- src/GenericComponent.jsx
+++ src/GenericComponent.jsx
@@ -1,6 +1,11 @@
 import "./GenericComponent.css";
 
 function GenericComponent() {
-  return <div className="component"></div>;
+  return (
+    <div className="component">
+      {/* debug 30ch lenght */}
+      <p>000000000000000000000000000000</p>
+    </div>
+  );
 }
 export default GenericComponent;
