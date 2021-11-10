d3ea222a1f6e950ac4101e2411e094443c08e663
Matteo <matteo.spina@hotmail.com>
2021-11-10 18:26:03 +0100

    comments

--- src/Items.jsx
+++ src/Items.jsx
@@ -1,32 +1,34 @@
 import styled, { css } from "styled-components/macro";
 
-/* Structural component: defines a grid item */
+/* 
+  "Grid Item" - Structural component: defines a generic grid item
+  a grid item has only one component as child
+*/
 const StyledGridItem = styled.div`
-  /* default [2row X 1col] */
+  /* default size: [2row X 1col] */
   grid-row: ${(props) => (props.halfOnY ? "span 1" : "span 2")};
   grid-column: ${(props) => (props.fullOnX ? "1 / -1" : "span 1")};
   padding: 1rem;
-  /* debug */
-  /* border: solid thin; */
-  /* background: lightcoral; */
 `;
 
-// helper
-// param: color object
+/* 
+  "Tile" - Decorative component: it carries styles for a tile-looking div
+*/
+// helper for StyledTile receive color object as arg
 const BackgroundTemplate = css`
-  background: linear-gradient(
-    307deg,
-    ${(props) => props.colorA || "var(--color-node-2)"} 0%,
-    ${(props) => props.colorB || "var(--color-node-1)"} 80%
+  background: radial-gradient(
+    circle farthest-corner at top left,
+    ${(props) => props.colorA || "var(--color-node-1)"} 0%,
+    ${(props) => props.colorB || "var(--color-node-2)"} 150%
   );
 `;
 
-/* Decorative component: wraps styles */
-// WARNING: sizes!!
+// WARNING: it does not define sizes!
 const StyledTile = styled.div`
   ${BackgroundTemplate};
   box-shadow: 0px 2px 2px 0px var(--color-shadow-1),
     0px 2px 8px 0px var(--color-shadow-2);
+  /* border radius are fixed */
   border-radius: 8px;
   border: 2px solid black;
 `;
--- src/Temporary.jsx
+++ src/Temporary.jsx
@@ -1,6 +1,9 @@
 import styled from "styled-components/macro";
 import { GridItem, Tile } from "./Items";
 
+/* 
+  list items are divs with a tile-looking
+*/
 const StyledListItem = styled(Tile)`
   width: 100%;
   height: 8ch;
@@ -15,6 +18,11 @@ function ListItem({ item }) {
   );
 }
 
+/* 
+  List is a container for list items
+  it holds css for centering children but does not have decorative css (borders, shadows, ...)
+  note: check component sizes
+*/
 const StyledList = styled.div`
   display: flex;
   flex-direction: column;
@@ -23,6 +31,7 @@ const StyledList = styled.div`
 
 function List({ items }) {
   return (
+    // placed directly on the grid?
     <GridItem>
       <StyledList>
         {items.map((item) => (
