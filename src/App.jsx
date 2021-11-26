2e9fb8d0b1eb28cea2f5b95cdc4e72e011b0be43
Matteo <matteo.spina@hotmail.com>
2021-11-26 17:55:36 +0100

    comments on typography

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Header, Footer, Main } from "./Sections";
-import styled, { StyleSheetManager, createGlobalStyle } from "styled-components";
+import styled, {
+  StyleSheetManager,
+  createGlobalStyle,
+} from "styled-components";
 import StyledScaleGrid from "./Scale";
 
 const GlobalStyle = createGlobalStyle`
@@ -8,42 +11,44 @@ const GlobalStyle = createGlobalStyle`
     font-family: "RubikLight";
     font-style: normal;
     font-weight: normal;
-    src: 
-      local("Rubik Light"),
-      local(Rubik Light),
-      local(Rubik-Light);
+    src: local("Rubik Light"), local(Rubik Light), local(Rubik-Light);
   }
-  
+
   :root {
-    /* fonts */
-    /* --font-large: 16px;
-    --font-medium: 16px;
-    --font-small: 16px; */
-    --line-height-custom: 2;
     /* colors */
     --color-body: orange;
     --color-node-1: #339933;
     --color-node-2: #66cc33;
     --color-react: #61dafb;
     --color-react-2: #282c34;
-	  --cra-color-primary: #09d3ac;
-	  --cra-color-background: #303846;
+    --cra-color-primary: #09d3ac;
+    --cra-color-background: #303846;
     --sc-color-primary: palevioletred;
-    --sc-background: rgba(0, 0, 0, 0) linear-gradient(20deg, rgb(33, 33, 33), rgb(66, 66, 66)) repeat scroll 0% 0%;
+    --sc-background: rgba(0, 0, 0, 0)
+      linear-gradient(20deg, rgb(33, 33, 33), rgb(66, 66, 66)) repeat scroll 0%
+      0%;
     --color-shadow-1: hsla(0, 11%, 20%, 0.74);
     --color-shadow-2: hsla(3, 100%, 50%, 0.66);
     --color-border: black;
-    /* shadows */    
+    /* shadows */
     --card-shadow-1: 0px 2px 2px 0px var(--color-shadow-1);
     --card-shadow-2: 0px 2px 8px 0px var(--color-shadow-2);
     /* backgrounds */
-    --node-gradient: linear-gradient(307deg,var(--color-node-2) 0%, var(--color-node-1) 80%);
-    --card-gradient: linear-gradient(90deg,rgba(210,191,191,0.29) 0%, rgba(226,215,215,0.12) 80%);
+    --node-gradient: linear-gradient(
+      307deg,
+      var(--color-node-2) 0%,
+      var(--color-node-1) 80%
+    );
+    --card-gradient: linear-gradient(
+      90deg,
+      rgba(210, 191, 191, 0.29) 0%,
+      rgba(226, 215, 215, 0.12) 80%
+    );
     /* borders */
     --border-width: 2px;
     --card-border: var(--border-width) solid var(--color-border);
     --card-border-radius: 8px;
-      
+
     box-sizing: border-box;
     /* font-size: 100%;  */
   }
@@ -53,7 +58,7 @@ const GlobalStyle = createGlobalStyle`
       /* font-size: var(--font-medium); */
     }
   }
-  
+
   @media (max-width: 400px) {
     html {
       /* font-size: var(--font-small); */
@@ -66,11 +71,78 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    /* theme */
+    /* typography */
+    /* 
+      Rubik 
+        em-square: 1000
+        ascender: 935; descender: 250; line-gap: 0;
+        tot: 935 + 250 + 0 = 1185 => R= 1.185
+        example: 
+          let 'font-size: 16px' which corresponds to 1000 ems 
+          you get: 16px * 1.185 = 18.96px ~ 19px (chars height)
+          let 'line-height: 2' ===> font-size(16px) * 2 = 32px (line-box height)
+        NOTA: font-size * line-height >= font-size * R
+          ==> line-height >= R (= 1.185)
+    */
+    --default-line-height: 2;
     font-family: "RubikLight", Verdana, sans-serif;
     font-weight: 400;
-    line-height: var(--line-height-custom);
+    line-height: var(--default-line-height);
+    /*  */
     background: var(--color-body);
+  }
+
+  /* 
+    impose a font-size and a line-height to specific elements, 
+    prevent browser to apply default styling 
+ 
+    - Typographic Scale -
+    
+    scale: Major Second
+    ratio: 1.125 (9:8)
+    base: 1rem
+    >> font-size := (ratio)^n * (base)
+    
+    - Grid -
+    
+    grid unit: 1rem (== base) 
+    >> line-height := k * (grid unit)
+    
+    - Constraint -
+    
+    on the same element
+    line-height >= R * font-size
+    
+  */
+  h1 {
+    /* n=6 */
+    font-size: 1.802rem;
+  }
+
+  h2 {
+    /*  */
+    font-size: 1.602rem;
+  }
+
+  h3 {
+    /*  */
+    font-size: 1.424rem;
+  }
+
+  h4 {
+    /* n=2 */
+    font-size: 1.266rem;
+  }
+
+  h5 {
+    /* n=1 */
+    font-size: 1.125rem;
+    /* k= */
+  }
+
+  small,
+  .text_small {
+    font-size: 0.889rem;
   }
 `;
 
@@ -82,7 +154,7 @@ const StyledApp = styled.div`
   grid-template-rows: [header-start] 5rem [main-start] 1fr [footer-start] 5rem;
   /* debug */
   position: relative;
-`
+`;
 
 function App() {
   return (
